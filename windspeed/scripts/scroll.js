3b80b6a10414a96b32eb4da10528541cd3720817
Tammy Zhang <tazhang@ucar.edu>
2023-04-05 14:41:28 -0400

    remove debugging indicators

--- windspeed/scripts/scroll.js
+++ windspeed/scripts/scroll.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         triggerElement: "#vidframe",
         triggerHook: 0
     })
-        .addIndicators()
+        // .addIndicators()
         .setPin("#vidframe")
         .addTo(controller)
     // implementing a minor delay after scrolling stops to make a smoother transition
@@ -32,91 +32,91 @@ $(document).ready(function() {
         duration: 5000,
         triggerHook: 0.35
     }).setPin("#trigger1")
-    .addIndicators();
+    // .addIndicators();
 
     var scene2 = new ScrollMagic.Scene({
         triggerElement: "#trigger2",
         duration: 8000,
         triggerHook: 0.35
     }).setPin("#trigger2")
-    .addIndicators();
+    // .addIndicators();
 
     var scene3 = new ScrollMagic.Scene({
         triggerElement: "#trigger3",
         duration: 8000,
         triggerHook: 0.35
     }).setPin("#trigger3")
-    .addIndicators();
+    // .addIndicators();
 
     var scene4 = new ScrollMagic.Scene({
         triggerElement: "#trigger4",
         duration: 9000,
         triggerHook: 0.35
     }).setPin("#trigger4")
-    .addIndicators();
+    // .addIndicators();
 
     var scene5 = new ScrollMagic.Scene({
         triggerElement: "#trigger5",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger5")
-    .addIndicators();
+    // .addIndicators();
 
     var scene6 = new ScrollMagic.Scene({
         triggerElement: "#trigger6",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger6")
-    .addIndicators();
+    // .addIndicators();
 
     var scene7 = new ScrollMagic.Scene({
         triggerElement: "#trigger7",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger7")
-    .addIndicators();
+    // .addIndicators();
 
     var scene7 = new ScrollMagic.Scene({
         triggerElement: "#trigger7",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger7")
-    .addIndicators();
+    // .addIndicators();
 
     var scene8 = new ScrollMagic.Scene({
         triggerElement: "#trigger8",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger8")
-    .addIndicators();
+    // .addIndicators();
 
     var scene9 = new ScrollMagic.Scene({
         triggerElement: "#trigger9",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger9")
-    .addIndicators();
+    // .addIndicators();
 
     var scene10 = new ScrollMagic.Scene({
         triggerElement: "#trigger10",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger10")
-    .addIndicators();
+    // .addIndicators();
 
     var scene11 = new ScrollMagic.Scene({
         triggerElement: "#trigger11",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger11")
-    .addIndicators();
+    // .addIndicators();
 
     var scene12 = new ScrollMagic.Scene({
         triggerElement: "#trigger12",
         duration: 7000,
         triggerHook: 0.35
     }).setPin("#trigger12")
-    .addIndicators();
+    // .addIndicators();
 
     controller.addScene([
         scene1,
